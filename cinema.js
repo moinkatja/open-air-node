0a69e721467e7ceb3dba049d1e60cc2590e89fed
kopikob <55636811+kopikob@users.noreply.github.com>
2020-08-24 12:59:40 +0200

    New Schema

--- cinema.js
+++ cinema.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CinemaSchema = new Schema({
+    id: Number,
     name: String,
     postcode: Number,
     city: String,
-    street: String
+    street: String, 
+    tel: String,
 })
 
 const Cinema = mongoose.model("cinema", CinemaSchema);
