14def77c6c7fe2d29c2249b68d9f379e4dfea110
kopikob <55636811+kopikob@users.noreply.github.com>
2020-09-11 15:16:09 +0200

    Backend for OpenAirKino App

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const bodyParser = require("body-parser");
 
 const app = express();
 var port = process.env.PORT || 4000;
-//databaseURL = "mongodb+srv://katjablau:HelloWorld2801@cluster0.pp2d8.mongodb.net/cinema-db?retryWrites=true&w=majority";
 
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
